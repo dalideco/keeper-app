7310dd0322cd21882db052bb0ecb0c3ef714e137
dali <dalissj1@gmail.com>
2021-01-07 22:32:58 +0100

    making form hooks

--- src/components/Myform.js
+++ src/components/Myform.js
@@ -1,13 +1,24 @@
-import React from 'react'
+import React,{useState} from 'react'
 import './Myform.scss'
 
 function Myform() {
+    const [title, setTitle]=useState("");
+    const [content , setContent]= useState("");
+
+    const changeTitle= (e)=>{
+        setTitle(e.target.value);
+    }
+
+    const changeContent= (e)=>{
+        setContent(e.target.value);
+    }
+
     return (
         <div className ="myform">
             <form>
-                <input name="title" placeholder="title">
+                <input name="title" placeholder="title" value={title} onChange={changeTitle}>
                 </input>
-                <textarea name="content" placeholder="content">
+                <textarea name="content" placeholder="content" value={content} onChange={changeContent}>
                 </textarea>
                 <button>Add</button>
             </form>
