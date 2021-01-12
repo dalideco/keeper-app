2492c646a4806196b2aaae2a0b3fe070c414bf89
dali <dalissj1@gmail.com>
2021-01-12 21:05:36 +0100

    changin delete icon

--- src/components/Block.js
+++ src/components/Block.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Block.scss';
 import Button from '@material-ui/core/Button';
-
+import CancelIcon from '@material-ui/icons/Cancel';
 
 function Block(props) {
 
@@ -16,10 +16,7 @@ function Block(props) {
             <form>
                 <h1>{props.title}</h1>
                 <p>{props.content}</p>
-                <Button variant="outlined" color ="secondary" onClick ={removeBlock}>
-                    x
-                </Button>
-
+                <CancelIcon onClick={removeBlock} color="secondary"/>
             </form>
         </div>
     )
