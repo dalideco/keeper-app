c12f65df672f22e94c1f2bc7dd9dbb42d8f018bc
dali <dalissj1@gmail.com>
2021-01-15 23:33:58 +0100

    adding material ui style

--- src/components/Block.js
+++ src/components/Block.js
@@ -1,7 +1,20 @@
 import React from 'react';
 import './Block.scss';
-import Button from '@material-ui/core/Button';
 import CancelIcon from '@material-ui/icons/Cancel';
+import {createMuiTheme} from '@material-ui/core/styles';
+import {green} from '@material-ui/core/colors';
+import {blue} from '@material-ui/core/colors';
+import {ThemeProvider} from '@material-ui/core/styles'
+
+const theme = createMuiTheme({
+    palette:{
+        primary:green,
+        secondary:{main:'#C34A36'}
+    },
+    status:{
+        danger:'orange'
+    }
+});
 
 function Block(props) {
 
@@ -13,11 +26,13 @@ function Block(props) {
     }
     return (
         <div className="block">
+            <ThemeProvider theme={theme}>
             <form>
                 <h1>{props.title}</h1>
                 <p>{props.content}</p>
                 <CancelIcon onClick={removeBlock} color="secondary"/>
             </form>
+            </ThemeProvider>
         </div>
     )
 }
